a3d006fbc8bca233102d01e849b725ddfd8c557a
JjelloCho <60625566+JjelloCho@users.noreply.github.com>
2020-02-04 14:10:54 -0600

    Update sketch.js

--- cs30-p5js-template copy/sketch.js
+++ cs30-p5js-template copy/sketch.js
@@ -5,11 +5,27 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let r = 0, b= 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  print(frameCount);
+  background(255);
+  fill(r,0,b);
+  ellipse(mouseX, mouseY, 90, 90);
+  noStroke();
+}
+function mousePressed(){
+  if(r===0){
+    r = 150;
+    b = 150;
+}
+  else {
+    r = 0;
+    b = 0;
+
+}
 }
