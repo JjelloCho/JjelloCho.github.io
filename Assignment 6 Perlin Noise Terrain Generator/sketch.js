889248c094abd7bfc46f5acdfc9af8956b605112
Helen Gee <jellotcho@gmail.com>
2020-03-06 21:21:14 -0600

    Added stuff

--- Assignment 6 Perlin Noise Terrain Generator/sketch.js
+++ Assignment 6 Perlin Noise Terrain Generator/sketch.js
@@ -6,11 +6,14 @@
 // - describe what you did to take this project "above and beyond"
 
 const widthRect =5;
-const sized= 10;
+const DT=0.03;
 let maxHeight;
 let y;
 let time;
-
+let yArray =[];//Keeps track of y Coordinates
+let xArray = [];//keeps track of x coordinates
+let greatestY, greatestX;//Keeps track the highest points
+let firstX =0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220);
@@ -20,23 +23,55 @@ function setup() {
 }
 
 function draw() {
+  // if(frameCount%25===0){
+  //   background(220);
+  //   generateTerrain();
+  // }
 
 }
 
 function generateTerrain(){
-  let lastY =[];
-  for(let x  =0; x<width; x+=3){
+  noStroke();
+  for(let x  =0; x<width; x+=widthRect-1){//draws the terrain
     rectMode(CORNERS);
     y = map(noise(time), 0,1, maxHeight, height);
-    time += 0.05;
+    time += DT;
     fill(0, 0, 255);
     rectMode(CORNERS);
     rect(x, height, x+widthRect, y);
-    lastY.push(y);
+    yArray.push(y);//adds to the arrays
+    xArray.push(x);
   }
-  for(let i =0, i<lst)
+  findY();
+  drawFlag();
+  average(); 
 }
-function drawFlag(x,y){
+
+//draws the flag
+function drawFlag(){
   fill(0);
-  rect(x, y, x+5, 20);
+  stroke(0);
+  strokeWeight(1);
+  line(greatestX+widthRect/2, greatestY-15, greatestX+widthRect/2, greatestY);
+  triangle(greatestX+widthRect/2,greatestY-17,greatestX+widthRect/2,greatestY-10,greatestX+widthRect+3,greatestY-13.5);
+}
+//finds the average
+function average(){
+  stroke(255, 0, 0);
+  let yAverage=0;
+  for(let i = 0; i<yArray.length;i++){
+    yAverage += yArray[i];
+  }
+  yAverage = yAverage/yArray.length;
+  line(0, yAverage, width, yAverage);
+}
+//finds the greatest y
+function findY(){
+  greatestY= yArray[0];
+  for(let i =0; i< yArray.length;i++){
+    if(yArray[i]< greatestY){
+      greatestY= yArray[i];
+      greatestX = xArray[i];
+    }
+  }
 }
