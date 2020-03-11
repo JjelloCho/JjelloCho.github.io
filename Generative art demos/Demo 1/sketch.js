e329f6d5f2a70986869e86fc6783cdfa53918ccb
JjelloCho <60625566+JjelloCho@users.noreply.github.com>
2020-03-11 13:22:08 -0600

    h

---
+++ Generative art demos/Demo 1/sketch.js
@@ -0,0 +1,15 @@
+// Generative Art Demo 01
+// Jordan Cho
+// 03/11/2020
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
