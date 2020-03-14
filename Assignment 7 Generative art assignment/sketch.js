a8804958b2604834dece6d4c1b48fa606c61bd24
Helen Gee <jellotcho@gmail.com>
2020-03-13 20:45:09 -0600

    Update sketch.js

--- Assignment 7 Generative art assignment/sketch.js
+++ Assignment 7 Generative art assignment/sketch.js
@@ -8,23 +8,39 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  stroke(0);
+  background(220);
+  //sinCurve();
+  push();
+  scale(0.75);
+  translate(width*0.15, 0);
+  multSin();
+  pop();
 }
 
 function draw() {
-  background(220);
-  sinCurve();
 }
 
 function sinCurve(){
-  let counter = 0;
-  strokeWeight(1);
-  for(let x = 0; x<width*0.25; x+= 0.1 ){
-    counter +=0.5;
+  let counter = 1;
+  strokeWeight(0.01);
+  for(let x = 0; x<width*0.2; x+= 0.01 ){
+    counter++;
+    let periodLength = map(x, 0, width*0.4, 2, 25);
     push();
-    translate(width*0.25, height*0.25);
-    scale(5);
-    line(x,3*sin(x), x, 3*sin(x));
+    //translate(width*0.15, height*0.25);
+    translate(0, height*0.25);
+    scale(100);
+    line(x/periodLength, 0.2*sin(x), x/periodLength, 0.2*sin(x));
     pop();
   }
+}
 
+function multSin(){
+  for( let i = 0; i<90; i++){
+    push();
+    translate(0, i*4);
+    sinCurve();
+    pop();
+  }
 }
